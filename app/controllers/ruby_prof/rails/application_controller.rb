91f56d85ef08ece66a77b130618173028eac31d9
Thomas Leishman <thomas.leishman@plansource.com>
2014-12-05 15:14:00 -0700

    Refactoring configuration from controller into configuration class

--- app/controllers/ruby_prof/rails/application_controller.rb
+++ app/controllers/ruby_prof/rails/application_controller.rb
@@ -1,42 +1,16 @@
 module RubyProf
   module Rails
     class ApplicationController < ActionController::Base
-      http_basic_authenticate_with name: RubyProf::Rails::Config.username, password: RubyProf::Rails::Config.password if RubyProf::Rails::Config.has_authentication?
-      before_filter :cache_class_enabled?, :disabled_alert
+      before_filter :properly_configured?
+      http_basic_authenticate_with RubyProf::Rails::Config.http_basic_authenticate if @enable_config
 
       private
 
-      def disabled_alert
-        flash[:alert] = nil
-        return if enabled_config?
-        has_authentication_alert unless has_authentication?
-        cache_class_enabled_alert unless cache_class_enabled?
+      def properly_configured?
+        return if @enable_config ||= RubyProf::Rails::Config.properly_configured?
+        flash[:alert] = ('<h4>Disabled: </h4><ul><li>' + RubyProf::Rails::Config.alerts.join('</li><li>') + '</li>').html_safe
       end
 
-      def enabled_config?
-        @enable_config = cache_class_enabled? && has_authentication?
-      end
-
-      def cache_class_enabled?
-        ::Rails.application.config.cache_classes
-      end
-
-      def cache_class_enabled_alert
-        flash[:alert] = %q{<p><b>Disabled: </b> To profile a Rails application it is vital to run it using production like settings
-                        (cache classes, cache view lookups, etc.). Otherwise, Rail's dependency loading code will overwhelm any time spent
-                        in the application itself (our tests show that Rails dependency loading causes a roughly 6x slowdown).</p>
-                        <p>To enable Rails Ruby Prof configuration you must initially set config.cache_classes = true.</p>}.html_safe
-        false
-      end
-
-      def has_authentication?
-        RubyProf::Rails::Config.has_authentication?
-      end
-
-      def has_authentication_alert
-        flash[:alert] = %q{<p><b>Disabled: </b> For security this page is disabled until RubyProf::Rails::Config.username
-                        and RubyProf::Rails::Config.password has been configured (see documentation). }.html_safe
-      end
     end
   end
 end
--- lib/ruby-prof/rails/config.rb
+++ lib/ruby-prof/rails/config.rb
@@ -2,8 +2,17 @@ module RubyProf
   module Rails
     # server configuration for RubyProfRails application
     class Config
+
+      CACHE_CLASS_ENABLED_ALERT = %q{<p>To profile a Rails application it is vital to run it using production like settings
+          (cache classes, cache view lookups, etc.). Otherwise, Rail's dependency loading code will overwhelm any time spent
+          in the application itself (our tests show that Rails dependency loading causes a roughly 6x slowdown).</p>
+          <p>To enable Rails Ruby Prof configuration you must initially set config.cache_classes = true.</p>}
+
+      HAS_AUTHENTICATION_ALERT = %q{<p>For security this page is disabled until RubyProf::Rails::Config.username
+          and RubyProf::Rails::Config.password has been configured (see documentation). }
+
       class << self
-        attr_accessor :username, :password, :content_types, :path
+        attr_accessor :username, :password, :path
 
         def path
           @path = ::Rails.root + 'tmp/performance' if @path.blank?
@@ -14,11 +23,31 @@ module RubyProf
           Hash(env['rack.session'][:ruby_prof_rails])
         end
 
+        def properly_configured?
+          cache_class_enabled? && has_authentication?
+        end
+
+        def cache_class_enabled?
+          ::Rails.application.config.cache_classes
+        end
+
         def has_authentication?
-          RubyProf::Rails::Config.username.present? || RubyProf::Rails::Config.password.present?
+          username.present? || password.present?
+        end
+
+        def alerts
+          alerts = []
+          alerts << CACHE_CLASS_ENABLED_ALERT unless has_authentication?
+          alerts << HAS_AUTHENTICATION_ALERT unless cache_class_enabled?
+          alerts
+        end
+
+        def http_basic_authenticate
+          {name: username, password: password}
         end
 
       end
+
     end
   end
 end

2dc4d01890ed6e7a54a54d537e2c2fd824c6a4cb
Thomas Leishman <thomas.leishman@plansource.com>
2015-01-23 16:15:39 -0700

    Add additional  methods to access profile data

--- lib/ruby-prof/rails/profile.rb
+++ lib/ruby-prof/rails/profile.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require_relative 'printers'
 
 module RubyProf
   module Rails
@@ -7,11 +8,11 @@ module RubyProf
 
       ID_PATTERN = [:time, :session_id]
 
-      attr_reader :filename
+      attr_reader :filename, :manifest
 
       def initialize(manifest_filename)
         @manifest = manifest_filename
-        @filename = hash[:filename]
+        @filename = RubyProf::Rails::Config.path + hash[:filename]
       end
 
       def exists?
@@ -31,12 +32,28 @@ module RubyProf
         Time.at(hash[:time].to_i)
       end
 
+      def friendly_time
+        time.strftime('%Y-%m-%d %I:%M:%S %Z')
+      end
+
+      def session_id
+        hash[:session_id]
+      end
+
       def id
         hash.slice(*ID_PATTERN).values.join('-')
       end
 
+      def url
+        hash[:url]
+      end
+
       def hash
         @hash ||= YAML::load_file @manifest
+      end
+
+      def printer
+        RubyProf::Rails::Printers.find_type_by(hash[:format])
       end
 
     end

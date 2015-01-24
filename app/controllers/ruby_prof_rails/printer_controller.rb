59d7f062263cc969776b96039c550a034efd332e
Thomas Leishman <thomas.leishman@plansource.com>
2015-01-24 13:19:21 -0700

    Namespace fix for Printer Controller

--- app/controllers/ruby_prof_rails/printer_controller.rb
+++ app/controllers/ruby_prof_rails/printer_controller.rb
@@ -1,29 +1,27 @@
 module RubyProfRails
-  module Rails
-    class PrinterController < RubyProfRails::ApplicationController
+  class PrinterController < RubyProfRails::ApplicationController
 
-      RUBY_PROF_GEM_DIR = Gem::Specification.find_by_name('ruby-prof').gem_dir
+    RUBY_PROF_GEM_DIR = Gem::Specification.find_by_name('ruby-prof').gem_dir
 
-      def show
-        printer = printers[params[:id].to_sym] || 'no-printer'
-        printer_path = File.join(RUBY_PROF_GEM_DIR, 'examples', printer)
+    def show
+      printer = printers[params[:id].to_sym] || 'no-printer'
+      printer_path = File.join(RUBY_PROF_GEM_DIR, 'examples', printer)
 
-        if printer && File.exist?(printer_path)
-          send_file printer_path
-        else
-          render text: 'Printer example was not found.' # write some content to the body
-        end
+      if printer && File.exist?(printer_path)
+        send_file printer_path
+      else
+        render text: 'Printer example was not found.' # write some content to the body
       end
-
-      private
-
-      def printers
-        RubyProf::Rails::Printers.hash.merge(
-          FlatPrinterWithLineNumbers: 'flat.txt',
-          CallTreePrinter: 'multi.grind.dat'
-        )
-      end
-
     end
+
+    private
+
+    def printers
+      RubyProf::Rails::Printers.hash.merge(
+        FlatPrinterWithLineNumbers: 'flat.txt',
+        CallTreePrinter: 'multi.grind.dat'
+      )
+    end
+
   end
 end
